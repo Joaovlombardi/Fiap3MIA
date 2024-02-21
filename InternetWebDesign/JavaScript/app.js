d968cdbcfba312a3990bd75f65bf6e827d623772
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-21 10:02:21 -0300

    Inicio

---
+++ InternetWebDesign/JavaScript/app.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')()
+
+/* if(true){
+    let nome1 = "Jv"
+    return no
+}
+console.log(nome1) */
+
+/* const nome1 = 'Jv'
+console.log(`${nome1 === 'José'? 'Olá' : 'Não te conhço'}`) */
+
+/* let nome1 = 'Virgolini'
+console.log(nome1.replace('V','C')) */
+
+console.log(Math.floor(Math.random()* 10))
